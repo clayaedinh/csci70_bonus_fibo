52f00a655880ba8731f6bfdd6ba4d36e84c79fc9
Aedin Clay <clayaedinh@gmail.com>
2022-12-06 22:25:47 +0800

    finished code

--- fibo.erl
+++ fibo.erl
@@ -1,1 +1,12 @@
-%empty
+fibonacci(X) ->
+    if
+        X < 2 ->
+            1;
+        true ->
+            fibonacci(X - 1) + fibonacci(X - 2)
+    end.
+
+main(_) ->
+    N = 9,
+    io:format("~10..0B~n", [fibonacci(N)]).
+
